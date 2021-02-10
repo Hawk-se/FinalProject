f159fae03544867679cc0c2dbae31a28609b8c77
Sahin Umsu <sahinumsu@gmail.com>
2021-02-10 22:04:34 +0100

    10th Feb - End of day

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,8 @@ namespace Business.Constants
 {
     public static class Messages
     {
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed="Ürün listelendi";
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
     }
